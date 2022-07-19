1281e1a2ab51dec6e5254258a7bbce91a72d32f8
hansol2943 <hansol2943@naver.com>
2022-07-19 23:14:08 +0900

    22-07-19

---
+++ BJ/src/BJ/BJ11931.java
@@ -0,0 +1,28 @@
+package BJ;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+
+public class BJ11931 {
+
+		public static void main(String[] args) throws Exception {
+			BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter sc2 = new BufferedWriter(new OutputStreamWriter(System.out));
+			Integer N[]=new Integer[Integer.parseInt(sc.readLine())];
+			for (int i = 0; i < N.length; i++) {
+				N[i]=Integer.parseInt(sc.readLine());
+			}
+			Arrays.sort(N,Collections.reverseOrder());
+			for (int i = 0; i < N.length; i++) {
+			sc2.write(N[i]+"\n");
+			}
+			sc2.flush();
+		}
+}
